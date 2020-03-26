3e9d67f5af240027bdff1e1aeb51e997e2edeb1a
Murillo Azambuja Gonçalves <murilloagoncalves@gmail.com>
2020-03-25 23:21:38 -0300

    Add a simple Command pattern

---
+++ Command/vendor/GarageDoor.java
@@ -0,0 +1,16 @@
+public class GarageDoor {
+    public void up() {
+    }
+
+    public void down() {
+    }
+
+    public void stop() {
+    }
+
+    public void lightOn() {
+    }
+
+    public void lightOff() {
+    }
+}
---
+++ Command/vendor/Light.java
@@ -0,0 +1,9 @@
+public class Light {
+    public void on() {
+        // turns light on...
+    }
+
+    public void off() {
+        // turns light off...
+    }
+}
